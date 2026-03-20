5f0381a3eabe0d8205ddf9aed0ea78b16c3a265a
Manjushri Shetty <manjushrishetty38@gmail.com>
2026-03-20 13:41:59 +0530

    Update vectorstore.py

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,20 +1,12 @@
-from langchain_community.vectorstores import FAISS
-from utils.loaders import load_all_documents
-from models.embeddings import get_embeddings
+# TEMP VECTORSTORE (NO FAISS)
+
+from .loaders import load_all_documents
 
 
 def create_vectorstore():
     documents = load_all_documents("data")
-    embeddings = get_embeddings()
-
-    vectorstore = FAISS.from_documents(documents, embeddings)
-
-    # Save locally
-    vectorstore.save_local("faiss_index")
-
-    return vectorstore
+    return documents
 
 
 def load_vectorstore():
-    embeddings = get_embeddings()
-    return FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    return None
