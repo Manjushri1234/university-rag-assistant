8ded29a878283a4541d46dc58619fa834dd32798
Manjushri Shetty <manjushrishetty38@gmail.com>
2026-03-20 13:49:52 +0530

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,10 +1,21 @@
-from langchain_groq import ChatGroq
-from config.config import GROQ_API_KEY
+import os
+from groq import Groq
 
 
 def get_llm():
-    return ChatGroq(
-        groq_api_key=GROQ_API_KEY,
-        model="llama-3.1-8b-instant",
-        temperature=0.3
-    )
+    api_key = os.getenv("GROQ_API_KEY")
+    client = Groq(api_key=api_key)
+
+    def generate_response(prompt):
+        response = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {"role": "system", "content": "You are a helpful university assistant."},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=0.3
+        )
+
+        return response.choices[0].message.content
+
+    return generate_response
